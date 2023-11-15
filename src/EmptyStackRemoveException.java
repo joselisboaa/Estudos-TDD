f0ead84f8ecd8472083b54b06ad7430dd27fd475
joselisboaa <joselisboaa05@gmail.com>
2023-11-15 17:41:39 -0300

    feat: make exceptions to use in tests cases

---
+++ src/EmptyStackRemoveException.java
@@ -0,0 +1,10 @@
+
+
+public class EmptyStackRemoveException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public EmptyStackRemoveException(String message) {
+		super(message);
+	}
+}
---
+++ src/FullStackAddException.java
@@ -0,0 +1,8 @@
+
+public class FullStackAddException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public FullStackAddException(String message) {
+		super(message);
+	}
+}
